5e0c54bc148159fe6e9db2b2c4a11aeba4c74754
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2022-07-10 07:56:48 -0300

    For without lambda

---
+++ technology/java/8-17/8/src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+	private int kilometers;
+
+	public Car(int kilometers) { this.kilometers = kilometers; }
+
+	public int getKilometers() {
+		return kilometers;
+	}
+}
+
--- technology/java/8-17/8/src/Main.java
+++ technology/java/8-17/8/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.*;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("foo");
+		List<Car> cars = Arrays.asList(new Car(300), new Car(80000), new Car(1900));
+		List<Car> oldWayCars = ForWithoutLambda.findCarsOldWay(cars);
+		for (Car car : oldWayCars) {
+			System.out.println(car.getKilometers());
+		}
 	}
+
+	public class ForWithoutLambda {
+		public static List<Car> findCarsOldWay(List<Car> cars) {
+			List<Car> selectedCars = new ArrayList<>();
+			for (Car car : cars) {
+				if (car.getKilometers() < 50000) {
+					selectedCars.add(car);
+				}
+			}
+			return selectedCars;
+		}
+	}
+
 }
+
